a323fbbb6f342687c4ff2f50b491da103880aada
Pablo Geyer <geyerpablo2@gmail.com>
2025-11-30 16:18:40 -0300

    chore: remove docstrings from PurchasesAppConfig class in apps.py

--- purchases/app/apps.py
+++ purchases/app/apps.py
@@ -1,12 +1,7 @@
-"""
-App configuration for purchases application.
-"""
 from django.apps import AppConfig
 
 
 class PurchasesAppConfig(AppConfig):
-    """Configuration for the purchases app."""
-
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
     verbose_name = "Purchases"
