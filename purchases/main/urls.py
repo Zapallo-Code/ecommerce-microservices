9a100b9fad16584b44b097e753d2a6cac9e54a72
Pablo Geyer <geyerpablo2@gmail.com>
2025-11-30 16:24:32 -0300

    chore: remove unnecessary docstrings and standardize string quotes in settings.py, urls.py, and wsgi.py for clarity

--- purchases/main/urls.py
+++ purchases/main/urls.py
@@ -1,9 +1,5 @@
-"""
-URL configuration for main project.
-"""
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
